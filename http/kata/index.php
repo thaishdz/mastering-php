8bc3b89d3c72f6c47faf339154935c269242ac79
Thais Hernández <thaishdzlpz@gmail.com>
2024-08-07 20:42:53 +0100

    Update index.php

--- http/kata/index.php
+++ http/kata/index.php
@@ -3,6 +3,7 @@
 require_once 'vendor/autoload.php';
 require_once 'src/Pokemon.php';
 
+use GuzzleHttp\Exception\RequestException;
 
 /*
  * DIFICULTAD EXTRA (opcional):
@@ -12,30 +13,60 @@ require_once 'src/Pokemon.php';
  * - Muestra el nombre, id, peso, altura y tipo(s) del Pokémon ✔️
  * - Muestra el nombre de su cadena de evoluciones ❓
  * - Muestra los juegos en los que aparece ✔️
- * - Controla posibles errores
+ * - Controla posibles errores 
  */
 
 
-function getPokemonDataAPI(string|int $name): Pokemon
+ /*
+ * TODO: 
+ * Preguntar chatgpt qué es PSR7
+ * 
+*/
+
+
+$client = new \GuzzleHttp\Client([
+    "base_uri" => "https://pokeapi.co/api/v2/",
+    "timeout"  => 5.0 // 5 sg
+]);
+
+function fetchPokemonData(string|int $identifier): Pokemon
 {
-    //TODO: try-catch por si p.e falla la conexión al servidor
-    $client = new \GuzzleHttp\Client();
-    $HTTP_GET_METHOD = 'GET';
-    $BASE_URL = "https://pokeapi.co/api/v2/pokemon/{$name}";
+    global $client;
+    $ENDPOINT = "pokemon/{$identifier}";
+    try {
+        $response = $client->request('GET', $ENDPOINT);
+        $evolutionChain = getEvolutionChain($identifier);
+        $pokemon_data = json_decode($response->getBody(), true); // 'true', convierte el JSON en un array asociativo
 
-    $response = $client->request($HTTP_GET_METHOD, $BASE_URL);
-    $pokemon_data = json_decode($response->getBody(), true); // 'true', convierte el JSON en un array asociativo
+        return new Pokemon($pokemon_data);
+    } catch (RequestException $e) {
+        echo "RequestException " . $e->getMessage();
+    }
 
-    return new Pokemon($pokemon_data);
 }
 
 
+function getEvolutionChain(string|int $identifier): array
+{
+    global $client;
+    $ENDPOINT = "pokemon-species/{$identifier}";
+
+    try {
+        $response = $client->request('GET', $ENDPOINT);
+        $evolutionResponse = json_decode($response->getBody(),true);
+        $evolutionChainURI = $evolutionResponse["evolution_chain"]["url"];
+
+        $response = $client->request('GET', substr($evolutionChainURI, -18));
+        $evolutionChain = json_decode($response->getBody(), true));
+        //TODO: devolver array con evoluciones
+        return [];
+    } catch (RequestException $e) {
+        echo "RequestException " . $e->getMessage();
+    }
+}
 
 
-$pokemon_1 = 'pikachu';
-$pokemon_2 = 'charizard';
-$pokemon_4 = 666;
-
-$pokemon = getPokemonDataAPI($pokemon_1);
+$input = readline('Search Pokemon: '); // Recoge el nombre o el id del pokémon por CLI
+$pokemon = fetchPokemonData($input);
 
 echo($pokemon);
