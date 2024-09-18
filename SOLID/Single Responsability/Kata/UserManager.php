012ac628dda482c60faaf537ec8abe785f713815
Thais Hernández <thaishdzlpz@gmail.com>
2024-09-18 19:24:36 +0100

    Update UserManager.php

--- SOLID/Single Responsability/Kata/UserManager.php
+++ SOLID/Single Responsability/Kata/UserManager.php
@@ -1,5 +1,5 @@
-
 <?php
+
 require_once('./User.php');
 
 // Se encarga de manejar tareas con usuarios
@@ -11,6 +11,14 @@ class UserManager
     public function add(User $user): void 
     {
        $this->users[] = $user;
+    }
+
+    function exists(string $id): array
+    {
+        return array_filter($this->users(), function ($user) use ($id)
+        {
+            return $user->id() === $id;
+        });
     }
 
     public function users(): array
